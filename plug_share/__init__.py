fbcdc51893bce771e5cc5dfd1a92ae551a7b63e3
Marvyn95 <marvinluyombya@gmail.com>
2024-11-26 20:46:16 +0300

    minor changes

--- plug_share/__init__.py
+++ plug_share/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restful import Api
 import pymongo
-from pymongo import MongoClient
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import json
 
